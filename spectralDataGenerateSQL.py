94f4e2795068b96cdeaba9197c283089dc8f59b6
Lily Donaldson <lilykdonaldson@gmail.com>
2019-10-06 13:50:40 -0400

    Update spectralDataGenerateSQL.py

--- spectralDataGenerateSQL.py
+++ spectralDataGenerateSQL.py
@@ -1,2 +1,6 @@
-for x in range(350,751):
-	print("`"+str(x)+"`"+"     decimal(3,2) NOT NULL,") 
+print("ALTER TABLE `Spectral Data`")
+for x in range (350,751):
+	#print("`"+str(x)+"`"+"     decimal(3,2) NOT NULL,") 
+	print("ADD CHECK (`"+str(x)+"`>=0),")
+	print("ADD CHECK (`"+str(x)+"`<=1),")
+print ";"
